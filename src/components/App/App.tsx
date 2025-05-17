9d242ef4246e3d228d09e0d88675660eac4658f4
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-05-17 11:09:27 +0300

    Move types folder to new location

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
-import type { Votes, VoteType } from "../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
 import { useState, type JSX } from "react";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
